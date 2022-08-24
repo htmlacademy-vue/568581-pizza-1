468f3aaefb75a8d55261be8ac439d1129487e416
NekoStalker <noghri@mail.ru>
2022-08-24 16:41:05 +0300

    lin commit

--- Makefile
+++ Makefile
@@ -1,17 +1,18 @@
 # Makefile for local development
 
+
 start_project:
-	docker-compose down -v
-	docker-compose up --build
+	docker compose down -v
+	docker compose up --build 
 
 run_server:
-	docker-compose down -v
-	docker-compose up backend --build
+	docker compose down -v
+	docker compose up backend --build
 
 run_tests:
 	cd src/frontend; echo "...Starting frontend tests" && \
 	npm run test:unit
 
 run_server:
-	docker-compose down -v
-	docker-compose up backend
+	docker compose down -v
+	docker compose up backend
--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,17 +1,36 @@
 <template>
-  <div id="app">
-    <Index/>
+  <div id="App">
+    <Index></Index>
   </div>
+  <!-- <div id="app">
+    <div class="main">
+      <div class="main__wrapper">
+        <div class="main__header">
+          <img
+            src="@/assets/img/logo.svg"
+            width="300"
+            height="47"
+            alt="V!U!E! Pizza"
+          />
+        </div>
+        <h1>Добро пожаловать!</h1>
+        <p>
+          Это проект V!U!E! Pizza для обучения на профессиональном
+          онлайн‑курсе<br />
+          <b>«Vue.js для опытных разработчиков».</b>
+        </p>
+      </div>
+    </div>
+  </div> -->
 </template>
 
 <script>
-import  Index from '@/views/Index';
-
+import Index from "@/views/Index.vue";
 export default {
   name: "App",
   components: {
-    Index
-  }
+    Index,
+  },
 };
 </script>
 
